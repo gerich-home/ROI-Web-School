7facd4b8d989f7387ef46afbfd0f03e9dd24f2ad
NikVayne <nik.vayne@yandex.ru>
2014-12-18 12:34:29 +0300

    Create files and folders

--- Docs/Practice/fileExplorer/yours/app.js
+++ Docs/Practice/fileExplorer/yours/app.js
@@ -1,50 +1,48 @@
 $(function() {
-	var name = fileSystem.name;
 
-	var root = $("#folder")
-		.clone()
-		.removeAttr("id")
-		.appendTo("#tree")
-		.find(".caption")
-		.text(name)
-		.end()
-		.click(function(event) {
-			event.stopPropagation();
-			var self = $(this);
-			var children = self.find(".list-group");
-			if(fileSystem.isLoaded) {
-				children.slideToggle();
-				return;
-			}
-			
-			self.addClass("loading");
-			
-			fileSystem.loadChildren().done(function() {
-				for(var i = 0; i < fileSystem.children.length; i++) {
-					if(fileSystem.children[i].isFolder) {
-						$("#folder")
-							.clone()
-							.removeAttr("id")
-							.appendTo(children)
-							.find(".caption")
-							//.data("file", fileSystem.children[i])
-							//later: .data("file").content
-							.text(fileSystem.children[i].name);
-					} else {
-						// TODO: add files
-                        $("#file")
-	                        .clone()
-		                    .removeAttr("id")
-		                    .appendTo(children)
-		                    .find(".caption")
-		                    .text(fileSystem.children[i].name);
-					}
-				}
+function getObject (obj, objId) {
+var object = $("#" + objId)
+.clone()
+.removeAttr("id")
+.find(".caption")
+.text(fileSystem.name)
+.end()
+return object;
+}
 
-				self.removeClass("loading");
-				children.slideDown();
-			});
-		});
-	
-	// TODO: add files
+function folders (obj){
+return getObject(obj, "folder")
+}
+function files (obj){
+return getObject(obj, "file");
+}
+
+folders(fileSystem).appendTo("#tree");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
