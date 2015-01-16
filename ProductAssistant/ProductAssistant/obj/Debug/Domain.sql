0ed89a280a1fb7e7af7d328eb3cfc3d6a4f1f930
Mary <PchelnikovaMasha@outlook.com>
2015-01-16 18:41:50 +0300

    Done

---
+++ ProductAssistant/ProductAssistant/obj/Debug/Domain.sql
@@ -0,0 +1,6 @@
+CREATE TABLE[IUser](
+	[Id][integer] NOT NULL,
+	[FirstName][varchar] NOT NULL,
+	[LastName][varchar] NOT NULL,
+	[Login][varchar] NOT NULL
+)
---
+++ ProductAssistant/ProductAssistant/obj/Debug/IUser.sql
@@ -0,0 +1,7 @@
+CREATE TABLE[IUser](
+	[Id][int] NOT NULL,
+	[FirstName][varchar]NOT NULL,
+	[SecondName][varchar] NOT NULL,
+	[Login][varchar] NOT NULL,
+	[Password][text] NOT NULL
+)
